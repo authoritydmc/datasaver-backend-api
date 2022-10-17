2b7cf4a7b46443d173a2da49171d036a374e9f61
Raj Dubey <com.mailuser@gmail.com>
2022-10-17 17:54:04 +0530

    Initial commit

---
+++ src/main/java/com/authoritydmc/datasaver/DatasaverApplication.java
@@ -0,0 +1,13 @@
+package com.authoritydmc.datasaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasaverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/authoritydmc/datasaver/DatasaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authoritydmc.datasaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
