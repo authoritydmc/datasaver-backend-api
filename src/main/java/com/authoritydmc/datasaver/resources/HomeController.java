d6536fb746d284d378ebc718fb80d6b63b7cb12d
Raj Dubey <com.mailuser@gmail.com>
2022-10-28 21:32:01 +0530

    1.Changed home controller to show api from properties file

--- src/main/java/com/authoritydmc/datasaver/resources/HomeController.java
+++ src/main/java/com/authoritydmc/datasaver/resources/HomeController.java
@@ -1,20 +1,27 @@
 package com.authoritydmc.datasaver.resources;
 
+import com.authoritydmc.datasaver.DTO.ResponseMessage;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin("http://localhost:4200")
 public class HomeController {
 
     @Value("${api_version}")
     private  String api_version;
     @GetMapping
-    public String welcome()
+    public ResponseEntity<ResponseMessage> welcome()
     {
 
-        return "Welcome to DataSaver Api ,Version : "+api_version;
+       var rf=new ResponseMessage( "Welcome to DataSaver Api ,Version : "+api_version);
+
+        return ResponseEntity.status(HttpStatus.OK).body(rf);
     }
 }
