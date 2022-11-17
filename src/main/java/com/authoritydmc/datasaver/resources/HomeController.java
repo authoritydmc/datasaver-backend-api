eee09a0668202af74f5afc0a00b54a444a73a668
Raj Dubey <com.mailuser@gmail.com>
2022-11-17 16:32:58 +0530

    updated Ping message

--- src/main/java/com/authoritydmc/datasaver/resources/HomeController.java
+++ src/main/java/com/authoritydmc/datasaver/resources/HomeController.java
@@ -2,6 +2,7 @@ package com.authoritydmc.datasaver.resources;
 
 import com.authoritydmc.datasaver.DTO.ResponseMessage;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,19 +10,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("/")
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HomeController {
-
+    @Value("${server.port}")
+    private int port;
     @Value("${api_version}")
     private  String api_version;
     @GetMapping
-    public ResponseEntity<ResponseMessage> welcome()
-    {
+    public ResponseEntity<Object> welcome() throws UnknownHostException {
+        HashMap<String,String> map=new HashMap<>();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String BASEurl="http://"+ip+":"+port+"/";
+        map.put("message"," Welcome to DataSaver Api ,Version : "+api_version +"<br>Available @ <a href='"+BASEurl+"' target='_blank'>"+BASEurl+"</a>");
 
-       var rf=new ResponseMessage( "Welcome to DataSaver Api ,Version : "+api_version);
+        map.put("BASE_URL",ip);
+        map.put("BASE_PORT",String.valueOf(port));
 
-        return ResponseEntity.status(HttpStatus.OK).body(rf);
+        return ResponseEntity.status(HttpStatus.OK).body(map);
     }
 }
